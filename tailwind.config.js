c36465090d22aaf3162082d03d77007034db949a
Moein Hosseini <rxmoein@tuta.io>
2023-09-11 22:46:55 +0200

    Added screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./apps/**/*.{html,ts}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
