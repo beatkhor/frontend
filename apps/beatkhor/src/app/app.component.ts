30e337f0e450c7fe52986076d2d7bd0244d420eb
Moein Hosseini <rxmoein@tuta.io>
2023-09-11 21:46:57 +0200

    Initial commit

---
+++ apps/beatkhor/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'beatkhor-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'beatkhor';
+}
