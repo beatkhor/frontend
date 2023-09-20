f4600dd3767d61e0ba0e52d05897ba4db29b1b67
no2ehi <graphist77@gmail.com>
2023-09-20 22:27:35 +0330

    ADD Grid Header Component template

---
+++ apps/beatkhor/src/app/shared/grid-header.component.ts
@@ -0,0 +1,27 @@
+import {Component, Directive, HostBinding, ViewEncapsulation} from '@angular/core'
+
+@Component({
+  selector: 'bk-gird-header',
+  template: ` <div class="post-grid-header flex items-center mb-6 lg:min-h-[36px]">
+    <ng-content select="[bk-section-divider-title]"></ng-content>
+    <div class="flex grow-[10] h-[.02rem] bg-neutral-700"></div>
+    <div
+      class="[&_a]:hidden md:[&_a]:flex md:[&_a]:no-underline md:[&_a]:items-center md:[&_a]:justify-center md:[&_a]:text-[.87rem]"
+    >
+      <ng-content select="[bk-section-divider-actions]"></ng-content>
+    </div>
+  </div>`,
+  styleUrls: ['./grid-header.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class GridHeaderComponent {}
+
+@Directive({
+  selector: '[bk-section-divider-actions]',
+})
+export class GridHeaderActionsDirective {}
+
+@Directive({
+  selector: '[bk-section-divider-title]',
+})
+export class GridHeaderTitleDirective {}
