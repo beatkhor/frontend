b1bcad75bb07f6f32ebe68473a6a97642c1ae919
no2ehi <graphist77@gmail.com>
2023-09-20 22:28:29 +0330

    ADD Grid Header Component to Shared Module

--- apps/beatkhor/src/app/shared/shared.module.ts
+++ apps/beatkhor/src/app/shared/shared.module.ts
@@ -5,10 +5,29 @@ import {NgModule} from '@angular/core'
 import {ConfirmDialogComponent} from './confirm-dialog.component'
 import {MaterialModule} from './material.module'
 import {NoticeComponent} from './notice.component'
+import {
+  GridHeaderActionsDirective,
+  GridHeaderComponent,
+  GridHeaderTitleDirective,
+} from './grid-header.component'
 
 @NgModule({
-  declarations: [ConfirmDialogComponent, NoticeComponent],
+  declarations: [
+    ConfirmDialogComponent,
+    NoticeComponent,
+    GridHeaderComponent,
+    GridHeaderActionsDirective,
+    GridHeaderTitleDirective,
+  ],
   imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
-  exports: [FormsModule, ReactiveFormsModule, MaterialModule, NoticeComponent],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    NoticeComponent,
+    GridHeaderComponent,
+    GridHeaderActionsDirective,
+    GridHeaderTitleDirective,
+  ],
 })
 export class SharedModule {}
