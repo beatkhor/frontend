cad58f2507ee5d93efc6e79c91cac833606d0756
no2ehi <graphist77@gmail.com>
2023-09-20 22:30:19 +0330

    Split index component template

--- apps/beatkhor/src/app/index/index.component.ts
+++ apps/beatkhor/src/app/index/index.component.ts
@@ -5,35 +5,7 @@ import {CallbackEvents} from '../core/models/callback-events'
 
 @Component({
   selector: 'bk-index',
-  template: `<div class="container mx-auto px-5">
-    <ng-container [ngSwitch]="notice">
-      <bk-notice
-        *ngSwitchCase="callbackEvents.AccountActivationSuccess"
-        [mode]="'success'"
-        [message]="'Awesome, your account is now activated!'"
-        (close)="onCloseNotice()"
-      ></bk-notice>
-
-      <bk-notice
-        *ngSwitchCase="callbackEvents.AccountActivationFailed"
-        [mode]="'error'"
-        [message]="
-          'OOps, We could not activate your account! Please contact the support.'
-        "
-        (close)="onCloseNotice()"
-      ></bk-notice>
-
-      <bk-notice
-        *ngSwitchCase="callbackEvents.ResetPasswordVerificationFailed"
-        [mode]="'error'"
-        [message]="
-          'OOps, We could not verify the password recovery link. Please try again or contact the support.'
-        "
-        (close)="onCloseNotice()"
-      ></bk-notice>
-    </ng-container>
-    <bk-header></bk-header>
-  </div>`,
+  templateUrl: './index.component.html',
 })
 export class IndexComponent implements OnInit {
   readonly callbackEvents = CallbackEvents
