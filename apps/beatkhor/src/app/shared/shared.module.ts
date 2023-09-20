a14a9bc1d90886c273ae9fac75c834bbf37b0d84
Ruhollah Nodehi <35344951+no2ehi@users.noreply.github.com>
2023-09-20 23:02:31 +0330

    Feature/section divider (#4)
    * ADD Grid Header Component template
    
    * ADD Grid Header Component Style
    
    * ADD Grid Header Component to Shared Module
    
    * ADD index component template
    
    * Split index component template
    
    * Updated main content style
    
    ---------
    
    Co-authored-by: rxmoein <rxmoein@tuta.io>

--- apps/beatkhor/src/app/shared/shared.module.ts
+++ apps/beatkhor/src/app/shared/shared.module.ts
@@ -6,16 +6,33 @@ import {GenreSelectorDialogComponent} from './genre-selector-dialog.component'
 import {TagSelectorDialogComponent} from './tag-selector-dialog.component'
 import {ConfirmDialogComponent} from './confirm-dialog.component'
 import {NoticeComponent} from './notice.component'
+import {
+  GridHeaderComponent,
+  GridHeaderTitleDirective,
+  GridHeaderActionsDirective,
+} from './grid-header.component'
 import {MaterialModule} from './material.module'
 
 @NgModule({
   declarations: [
     ConfirmDialogComponent,
+    NoticeComponent,
+    GridHeaderComponent,
+    GridHeaderActionsDirective,
+    GridHeaderTitleDirective,
     GenreSelectorDialogComponent,
     TagSelectorDialogComponent,
     NoticeComponent,
   ],
   imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
-  exports: [FormsModule, ReactiveFormsModule, MaterialModule, NoticeComponent],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    NoticeComponent,
+    GridHeaderComponent,
+    GridHeaderActionsDirective,
+    GridHeaderTitleDirective,
+  ],
 })
 export class SharedModule {}
