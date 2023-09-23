e72c6fab871b61864185b6314aee42d3e4044d07
Moein Hosseini <rxmoein@tuta.io>
2023-09-23 12:24:34 +0000

    Separated index header styles file

--- apps/beatkhor/src/app/index/index-header/index-header.component.ts
+++ apps/beatkhor/src/app/index/index-header/index-header.component.ts
@@ -3,18 +3,6 @@ import {Component} from '@angular/core'
 @Component({
   selector: 'bk-header',
   templateUrl: './index-header.component.html',
-  styles: [
-    `
-      .link {
-        @apply mr-4 mb-4 no-underline text-[0.87rem];
-      }
-
-      @media (min-width: 1024px) {
-        .description {
-          text-align: unset;
-        }
-      }
-    `,
-  ],
+  styleUrls: ['./index-header.component.scss'],
 })
 export class IndexHeaderComponent {}
