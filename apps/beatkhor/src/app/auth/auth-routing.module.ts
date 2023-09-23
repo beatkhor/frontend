7eba78bce79d82a1a9f63aaa72e3c906d29f5c41
Moein Hosseini <rxmoein@tuta.io>
2023-09-23 11:37:26 +0000

    Separate complete profile component

--- apps/beatkhor/src/app/auth/auth-routing.module.ts
+++ apps/beatkhor/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,7 @@
 import {RouterModule, Routes} from '@angular/router'
 import {NgModule} from '@angular/core'
 
-import {CompleteProfileComponent} from './auth-complete-profile.component'
+import {CompleteProfileComponent} from './auth-complete-profile/auth-complete-profile.component'
 import {ResetRequestComponent} from './auth-reset-request.component'
 import {SignInComponent} from './auth-sign-in.component'
 import {SignUpComponent} from './auth-sign-up.component'
--- apps/beatkhor/src/app/auth/auth.module.ts
+++ apps/beatkhor/src/app/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 
-import {CompleteProfileComponent} from './auth-complete-profile.component'
+import {CompleteProfileComponent} from './auth-complete-profile/auth-complete-profile.component'
 import {ResetRequestComponent} from './auth-reset-request.component'
 import {SignInComponent} from './auth-sign-in.component'
 import {SignUpComponent} from './auth-sign-up.component'
