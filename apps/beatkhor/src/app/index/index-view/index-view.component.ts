6544140c434840d39357d4548e0a8957b61616a1
rxmoein <rxmoein@tuta.io>
2023-09-27 19:18:27 +0200

    Index page completed

--- apps/beatkhor/src/app/index/index-view/index-view.component.ts
+++ apps/beatkhor/src/app/index/index-view/index-view.component.ts
@@ -1,7 +1,10 @@
 import {ActivatedRoute, NavigationExtras, Router} from '@angular/router'
 import {Component, OnInit} from '@angular/core'
+import {forkJoin, lastValueFrom} from 'rxjs'
 
 import {CallbackEvents} from '../../core/models/callback-events'
+import {PostService} from '../../core/services/post.service'
+import {Post} from '../../core/models/post'
 
 @Component({
   selector: 'bk-index-view',
@@ -9,12 +12,54 @@ import {CallbackEvents} from '../../core/models/callback-events'
 })
 export class IndexViewComponent implements OnInit {
   readonly callbackEvents = CallbackEvents
-  notice!: string | null
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  latestRecommendedPost: Post[] = []
+  latestDrillPost: Post[] = []
+  latestTrapPost: Post[] = []
+  notice!: string | null
+  loading = false
+
+  pageSize = 10
+  demoArr = Array(this.pageSize)
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
   ngOnInit(): void {
     this.notice = this.route.snapshot.queryParamMap.get('notice')
+    this.getData()
+  }
+
+  private async getData() {
+    const latestPostReq$ = this.postService.search({
+      pageSize: this.pageSize,
+      tags: [{slug: 'recommended'}],
+    })
+    const trapPostReq$ = this.postService.search({
+      pageSize: this.pageSize,
+      genres: [{slug: 'trap'}],
+    })
+    const drillPostReq$ = this.postService.search({
+      pageSize: this.pageSize,
+      genres: [{slug: 'drill'}],
+    })
+    const req$ = forkJoin([latestPostReq$, trapPostReq$, drillPostReq$])
+
+    try {
+      this.loading = true
+      const [latestResponse, trapPostResponse, drillPostResponse] = await lastValueFrom(
+        req$
+      )
+      this.latestRecommendedPost = latestResponse.result
+      this.latestTrapPost = trapPostResponse.result
+      this.latestDrillPost = drillPostResponse.result
+      this.loading = false
+    } catch (error) {
+      this.loading = false
+    }
   }
 
   onCloseNotice() {
